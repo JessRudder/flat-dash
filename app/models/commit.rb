7ab72f521bd2360d9bc47bd4bb7a18f8c846c604
JessRudder <thejessicarudder@gmail.com>
2014-09-08 21:13:41 -0400

    Changes to Commit that achieve nothing

--- app/models/commit.rb
+++ app/models/commit.rb
@@ -18,7 +18,7 @@ class Commit < ActiveRecord::Base
   #   student_repos.collect do |student|
   #     client.commits(student["student_repo_name"], student["branch"]).each do |commit_list|
   #       if commit_list.author
-  #         find_or_create_by(user_login: commit_list.author.login, name: commit_list.commit.author.name, commit_message: commit_list.commit.message, commit_created_at: commit_list.commit.committer.date)
+  #         find_or_create_by(programmer_id: commit_list.author.login, name: commit_list.commit.author.name, commit_message: commit_list.commit.message, commit_created_at: commit_list.commit.committer.date)
   #       end
   #     end
   #   end
@@ -28,8 +28,8 @@ class Commit < ActiveRecord::Base
   def self.top_commits_by_user
     commit_array = []
     Commit.order("commit_created_at DESC").maximum(:commit_message)
-    commit_dates = Commit.group(:user_login).order("commit_created_at DESC").maximum(:commit_created_at)
-    top_user_commits = Commit.group(:user_login).order("count_all DESC").calculate(:count, :all)
+    commit_dates = Commit.group(:programmer_id).order("commit_created_at DESC").maximum(:commit_created_at)
+    top_user_commits = Commit.group(:programmer_id).order("count_all DESC").calculate(:count, :all)
     users = top_user_commits.keys
     count = top_user_commits.values
     last_commit = commit_dates  
@@ -41,11 +41,11 @@ class Commit < ActiveRecord::Base
 
   #This method find the lastest commits by user and shows their commit message
   def self.latest_commit_messages
-    commit_user = Commit.group(:user_login).select(:id).order("commit_created_at DESC").limit(15).maximum(:commit_created_at)
+    commit_user = Commit.group(:programmer_id).select(:id).order("commit_created_at DESC").limit(15).maximum(:commit_created_at)
     commit_array = []
     commit_user.each do |user,date|
-       messages = Commit.where("commit_created_at >= ?", date).find_by "user_login = ?", user
-       commit_array << ({:sDate => date.strftime("%F"), :sTime =>date.strftime("%R"), :sUsername => messages.user_login, :sCommitMessage => messages.commit_message, :sTimeFrame => "EVERYONE"})
+       messages = Commit.where("commit_created_at >= ?", date).find_by "programmer_id = ?", user
+       commit_array << ({:sDate => date.strftime("%F"), :sTime =>date.strftime("%R"), :sUsername => messages.programmer_id, :sCommitMessage => messages.commit_message, :sTimeFrame => "EVERYONE"})
     end
     commit_array
   end
@@ -53,11 +53,11 @@ class Commit < ActiveRecord::Base
   # This method picks one user at a time and shows their last 10 commit messages
   def self.user_commits
     commit_array = []
-    user_logins = Commit.pluck(:user_login).uniq 
-    login = user_logins.sample
-    messages = Commit.where("user_login = ?", login).order("commit_created_at DESC").select(:user_login, :commit_message, :commit_created_at).limit(15)
+    programmer_ids = Commit.pluck(:programmer_id).uniq 
+    login = programmer_ids.sample
+    messages = Commit.where("programmer_id = ?", login).order("commit_created_at DESC").select(:programmer_id, :commit_message, :commit_created_at).limit(15)
     messages.each do |message|
-      commit_array << ({:sDate => message.commit_created_at.strftime("%F"), :sTime =>message.commit_created_at.strftime("%R"),:sUsername => message.user_login, :sCommitMessage => message.commit_message, :sTimeFrame => message.user_login})
+      commit_array << ({:sDate => message.commit_created_at.strftime("%F"), :sTime =>message.commit_created_at.strftime("%R"),:sUsername => message.programmer_id, :sCommitMessage => message.commit_message, :sTimeFrame => message.programmer_id})
     end
     commit_array
   end
