785c045b01e7214ade7858e9f31dbb2aaa9f0274
JessRudder <thejessicarudder@gmail.com>
2014-09-13 12:41:31 -0400

    Cleans up old code

--- app/models/commit.rb
+++ app/models/commit.rb
@@ -5,8 +5,6 @@ class Commit < ActiveRecord::Base
 
   #This method finds the top 10 users the highest commit count
   def self.top_commits_by_user
-
-    # Potentially useful code
     commit_array = []
     commit_dates = Commit.joins(:programmer).group("programmers.name").order("commit_created_at DESC").maximum(:commit_created_at)
     top_user_commits = Commit.joins(:programmer).group("programmers.name").order("count_all DESC").calculate(:count, :all)
@@ -14,14 +12,6 @@ class Commit < ActiveRecord::Base
     count = top_user_commits.values
     last_commit = commit_dates
     users.each_with_index do |user, i|
-    # commit_array = []
-    # Commit.order("commit_created_at DESC").maximum(:commit_message)
-    # commit_dates = Commit.group(:programmer_id).order("commit_created_at DESC").maximum(:commit_created_at)
-    # top_user_commits = Commit.group(:programmer_id).order("count_all DESC").calculate(:count, :all)
-    # users = top_user_commits.keys
-    # count = top_user_commits.values
-    # last_commit = commit_dates  
-    # users.each_with_index do |user, i|
       commit_array << ({:sDate => last_commit[user].strftime("%F"), :sTime => last_commit[user].strftime("%R"), :sUsername => user, :sTimeFrame => "week", :nCommits => count[i]})
     end
     commit_array
@@ -29,7 +19,7 @@ class Commit < ActiveRecord::Base
 
   #This method find the lastest commits by user and shows their commit message
   def self.latest_commit_messages
-     hhgghcommit_user = Commit.group(:branch_id).select(:id).order("commit_created_at DESC").limit(15).maximum(:commit_created_at)
+    commit_user = Commit.group(:branch_id).select(:id).order("commit_created_at DESC").limit(15).maximum(:commit_created_at)
     commit_array = []
     commit_user.each do |user,date|
        messages = Commit.where("commit_created_at >= ?", date).find_by "programmer_id = ?", user
